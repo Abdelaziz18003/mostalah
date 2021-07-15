cc47100c945c623926f53add46c37129fc8838dd
Abdelaziz Mokhnache <m.abdelaziz1993@gmail.com>
2021-07-15 23:00:14 +0100

    remove validators and controllers folders

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,9 @@
 var express = require('express')
 var router = express.Router()
-const termsRouter = require('../controllers/term.controller')
 
 /* GET home page. */
 router.get('/', function (req, res) {
   res.render('index')
 })
-
-router.use(termsRouter)
 
 module.exports = router
--- validators.js/updateTerm.validator.js
+++
@@ -1,6 +0,0 @@
-const { check } = require('express-validator')
-
-module.exports = [
-  check('from').not().isEmpty().isString(),
-  check('to').not().isEmpty().isString(),
-]
