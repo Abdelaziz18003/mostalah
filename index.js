6763077daa5838fe35ad7ecd40a206d726513a65
Abdelaziz Mokhnache <m.abdelaziz1993@gmail.com>
2025-01-13 02:04:45 +0100

    add ability to specify port via command args

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var http = require('http')
 const app = require('./web/app')
+const minimist = require('minimist')
 
-const PORT = process.env.PORT || 3000
+const argv = minimist(process.argv.slice(2))
+const PORT = argv.port || process.env.PORT || 3000
+
 const server = http.createServer(app)
 server.listen(PORT, () => {
   console.log('running on port', PORT)
